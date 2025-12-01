ec7593760a64fb554510476a065c87cbeb051463
Giulia004 <giuliafilicicchia@gmail.com>
2025-12-01 12:31:22 +0100

    Update Character class

--- rpg_game/src/main/java/com/game/model/Character.java
+++ rpg_game/src/main/java/com/game/model/Character.java
@@ -6,4 +6,16 @@ public class Character {
   private Stats stats;
   private String name;
   List<Ability> abilities;
+
+  public final Stats getStats() {
+    return this.stats;
+  }
+  
+  public final String getName() {
+    return this.name;
+  }
+  
+  public final List<Ability> getAbilities() {
+      return this.abilities;
+  }
 }
