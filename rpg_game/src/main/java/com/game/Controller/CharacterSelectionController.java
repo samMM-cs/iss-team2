a8903e129ee10315761a08e4e5259dbb9244f939
samMM-cs <morrealesamuele3@gmail.com>
2025-12-01 13:03:57 +0100

    updated characterselection

---
+++ rpg_game/src/main/java/com/game/Controller/CharacterSelectionController.java
@@ -0,0 +1,25 @@
+package com.game.Controller;
+
+import com.game.View.MainMenuView;
+
+import javafx.event.ActionEvent;
+import javafx.stage.Stage;
+
+public class CharacterSelectionController {
+  private Stage stage;
+
+  public CharacterSelectionController(Stage stage) {
+    this.stage = stage;
+  }
+
+  public void onArcher(ActionEvent e) {
+    System.out.println("Archer");
+    System.out.println(e);
+  }
+
+  public void onBack(ActionEvent e) {
+    System.out.println("Back");
+    System.out.println(e);
+    new MainMenuView(stage).show();
+  }
+}
--- rpg_game/src/main/java/com/game/View/CharacterSelectionView.java
+++ rpg_game/src/main/java/com/game/View/CharacterSelectionView.java
@@ -5,35 +5,45 @@ import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
+
+import com.game.Controller.CharacterSelectionController;
+import com.game.Controller.MainMenuController;
+
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.stage.*;
 
 public class CharacterSelectionView {
     private Stage stage;
+    private CharacterSelectionController controller;
 
     public CharacterSelectionView(Stage stage) {
         this.stage = stage;
+        this.controller = new CharacterSelectionController(stage);
     }
 
     public void selectCharacters() {
         VBox buttons = new VBox(15);
+        buttons.setAlignment(Pos.CENTER);
 
         ImageView imgSfondo = new ImageView(new Image(getClass().getResourceAsStream("/Forest.png")));
-        
+
         StackPane root = new StackPane(imgSfondo, buttons);
 
         // Personaggi
         // ImageView archer = new ImageView(new
         // Image(getClass().getResourceAsStream("/characters/archer.png")));
         Button archerBtn = new Button("Archer");
-        buttons.setAlignment(Pos.CENTER);
         buttons.getChildren().addAll(archerBtn);
-        archerBtn.setOnAction(e -> System.out.println("Hai selezionato Archer"));
+        archerBtn.setOnAction(controller::onArcher);
+
+        Button backBtn = new Button("Back");
+        buttons.getChildren().addAll(backBtn);
+        backBtn.setOnAction(controller::onBack);
         Scene scene = new Scene(root, 800, 600);
         imgSfondo.fitHeightProperty().bind(scene.heightProperty());
         imgSfondo.fitWidthProperty().bind(scene.widthProperty());
-        
+
         stage.setResizable(true);
         stage.setScene(scene);
         stage.show();
