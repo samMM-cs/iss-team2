8ed34b248d80b863142b614cd28fafc6324d2f84
samMM-cs <morrealesamuele3@gmail.com>
2026-01-10 15:33:47 +0100

    added json support to flagmap

--- rpg_game/src/main/java/com/game/model/story/FlagMap.java
+++ rpg_game/src/main/java/com/game/model/story/FlagMap.java
@@ -1,14 +1,22 @@
 package com.game.model.story;
 
 import java.util.EnumMap;
+import java.util.List;
+import java.util.Map.Entry;
+
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonValue;
 
 public class FlagMap {
     private final EnumMap<Flag, Boolean> flags;
 
     public FlagMap() {
-        /*this.flags = Arrays.stream(Flag.values())
-            .collect(Collectors.toMap(Function.identity(), Flag -> false, (a,b) -> a, () -> new EnumMap<>(Flag.class)));*/
-        this.flags= new EnumMap<>(Flag.class);
+        /*
+         * this.flags = Arrays.stream(Flag.values())
+         * .collect(Collectors.toMap(Function.identity(), Flag -> false, (a,b) -> a, ()
+         * -> new EnumMap<>(Flag.class)));
+         */
+        this.flags = new EnumMap<>(Flag.class);
         for (Flag Flag : Flag.values()) {
             flags.put(Flag, false);
         }
@@ -21,4 +29,22 @@ public class FlagMap {
     public void setFlag(Flag Flag, boolean flag) {
         this.flags.put(Flag, flag);
     }
+
+    @JsonCreator
+    public FlagMap(List<Flag> flags) {
+        this();
+        for (Flag flag : flags) {
+            this.flags.put(flag, true);
+        }
+    }
+
+    @JsonValue
+    // only save set flags in json
+    public List<Flag> getFlags() {
+        return flags.entrySet().stream()
+                .filter(Entry::getValue)
+                .map(Entry::getKey)
+                .toList();
+    }
+
 }
